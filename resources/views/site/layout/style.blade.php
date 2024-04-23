bd510495ecb6800d2619b3e082cbf4cbf312d716
juliano <jot.asa@hotmail.com>
2024-04-23 20:16:06 -0300

    home

--- resources/views/site/layout/style.blade.php
+++ resources/views/site/layout/style.blade.php
@@ -1,4 +1,4 @@
-@vite(['resources/sass/app.scss','resources/js/app.js'])
+@vite(['resources/sass/app.scss','resources/js/app.js','resources/css/app.css'])
 <header class="container-fluid p-3 mb-2 bg-body-secondary text-dark">
     @yield('top')
     <div class="row">
@@ -7,10 +7,10 @@
         </div>
         <div class="col">
             <div class="row">
-                <div class="col-3"><a class="icon-link icon-link-hover fs-5 link-underline-opacity-0" style="--bs-link-hover-color-rgb: 255, 0, 0;" href="{{route('board.index')}}">Home</a></div>
-                <div class="col-3"><a class="icon-link icon-link-hover fs-5 link-underline-opacity-0" style="--bs-link-hover-color-rgb: 255, 0, 0;" href="{{route('board.index')}}">Portifólio</a></div>
-                <div class="col-3"><a class="icon-link icon-link-hover fs-5 link-underline-opacity-0" style="--bs-link-hover-color-rgb: 255, 0, 0;" href="{{route('board.index')}}">Sobre</a></div>
-                <div class="col-3"><a class="icon-link icon-link-hover fs-5 link-underline-opacity-0" style="--bs-link-hover-color-rgb: 255, 0, 0;" href="{{route('board.index')}}">Contato</a></div>
+                <div class="col-3 transition ease-in-out delay-75 text-center hover:text-red-600 hover:scale-150 duration-300"><a class=" fs-5" href="{{route('board.index')}}">Home</a></div>
+                <div class="col-3 transition ease-in-out delay-75 text-center hover:text-red-600 hover:scale-150 duration-300"><a class=" fs-5" href="{{route('board.index')}}">Portifólio</a></div>
+                <div class="col-3 transition ease-in-out delay-75 text-center hover:text-red-600 hover:scale-150 duration-300"><a class=" fs-5" href="{{route('board.index')}}">Sobre</a></div>
+                <div class="col-3 transition ease-in-out delay-75 text-center hover:text-red-600 hover:scale-150 duration-300"><a class=" fs-5" href="{{route('board.index')}}">Contato</a></div>
             </div>
         </div>
     </div>
