58d0826481c46c86724440ff6dcae4784383a454
juliano <jot.asa@hotmail.com>
2024-04-24 11:03:07 -0300

    home_front_done

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -7,22 +7,22 @@
     @include('site.layout.style')
     <title>Portifólio</title>
 </head>
-<body>
+<body class="bg-white">
     @section('top')
     @endsection
 </body>
-<main class="container-sm flex items-stretch">
-    <div class="col-6 h-96 content-center mt-20 p-10">
+<main class="container-sm flex items-stretch bg-white">
+    <div class="col-5 h-96 content-center mt-20 p-5">
         <p class="fs-1">Bem-vindo,</p><br>
         <p class="fs-5">esse é meu website portifólio</p>
-        <button class="col-5 btn btn-outline-secondary btn-sm border-2 shadow-md rounded-full justify-center transition ease-in-out delay-75 hover:bg-red-600 hover:scale-125 duration-300 mt-3">Meu caminho</button>
-        <div class="col-5 flex justify-center mt-3 gap-2">
+        <button class="col-6 btn btn-outline-secondary btn-sm border-2 shadow-md rounded-full justify-center transition ease-in-out delay-75 hover:bg-red-600 hover:scale-125 duration-300 mt-3">Meu caminho</button>
+        <div class="col-6 flex justify-center mt-3 gap-2">
             <a class="shadow-lg" href="https://www.linkedin.com/in/juliano-gon%C3%A7alves-dos-santos-720b5623/" target="_blank" rel="external"><img src="https://cdn0.iconfinder.com/data/icons/social-network-7/50/9-512.png" width="48px" alt="linkedin"></a>
             <a class="shadow-lg" href="https://github.com/JulianoGSantos" target="_blank" rel="external"><img src="https://cdn3.iconfinder.com/data/icons/social-rounded-2/72/GitHub-512.png" width="48px" alt="github"></a>
         </div>
     </div>
-    <div class="col-6 h-96 content-center mt-20 p-10">
-        <img src="img/portifolio.png" width="1000px" alt="setup">
+    <div class="content-center size-full mt-20">
+        <img src="img/portifolio.png" width="100%" alt="setup">
     </div>
 </main>
 
